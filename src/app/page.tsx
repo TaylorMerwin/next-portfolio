80516ffeeb69b10c3bb0df16823eb17778ec05ad
Taylor <taylor.merwin@gmail.com>
2024-06-09 13:15:54 -0700

    added daisy theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
-    </main>
+    <>
+      <html data-theme="fantasy">
+    <h1 className="text-4xl font-bold text-center">Hello, world!</h1>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-secondary">Secondary</button>
+    </html>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,14 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  daisyui: {
+    themes: [
+      "fantasy", "sunset"
+    ],
   },
-  plugins: [daisyui],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('daisyui'),
+  ],
 };
 export default config;
