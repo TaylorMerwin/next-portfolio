4e1ddccc9cd70d5366b33a18db80d716a4b6c439
Taylor <taylor.merwin@gmail.com>
2024-06-12 14:19:54 -0700

    fixed typo

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -19,7 +19,7 @@ export default function Projects({ projects }: ProjectsProps) {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col md:items-center md:mt-8">
-      <h2 className="text-2xl font-bold ml-8">A few projects I've worked on</h2>
+      <h2 className="text-2xl font-bold ml-8">A few projects I have worked on</h2>
       {projects.map((project) => (
         <div key={project.title} className="mb-2">
           <ProjectCard {...project} />
